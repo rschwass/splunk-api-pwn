155ac1fa979ad7b62db051e7a1fa0f68aadce37a
rschwass <robertschwass@gmail.com>
2025-03-13 10:42:28 -0400

    fixed handlerfile

--- remove.sh
+++ remove.sh
@@ -2,3 +2,6 @@
 
 curl -k -u admin https://127.0.0.1:8089/services/apps/local/rt-win -X delete
 curl -k -u admin https://127.0.0.1:8089/servicesNS/admin/search/configs/conf-restmap/exec -X delete
+
+
+curl -k -u admin https://127.0.0.1:8089/services/server/control/restart -X POST
